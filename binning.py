51b3598977208c8835b3c7b80498b43702efa0f4
Paul Goldsmith-Pnikham <paulgp@gmail.com>
2014-01-02 12:26:23 -0600

    Add: Move Timer() to own file

--- binning.py
+++ binning.py
@@ -7,23 +7,8 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 from collections import Counter
 import time
-
-# Timer class taken from http://www.huyng.com/posts/python-performance-analysis/
-class Timer(object):
-    def __init__(self, verbose=False):
-        self.verbose = verbose
-
-    def __enter__(self):
-        self.start = time.time()
-        return self
-
-    def __exit__(self, *args):
-        self.end = time.time()
-        self.secs = self.end - self.start
-        self.msecs = self.secs * 1000  # millisecs
-        if self.verbose:
-            print 'elapsed time: %f ms' % self.msecs
-
+from timer import Timer
+from jellyfish import damerau_levenshtein_distance as ld
 
 # Got this from here: https://wiki.python.org/moin/BitManipulation
 # Adds up the number of bits that are "on"
@@ -33,7 +18,6 @@ def bitCount(int_type):
         int_type &= int_type - 1
         count += 1
     return(count)
-
 
 def removeDuplicates(stringList):
     return list(set(stringList))
@@ -49,22 +33,27 @@ def binCount(string,bins):
         coord.append(sum([stringCounter[z] for z in LETTERS[x:y]]))
     return tuple(coord)
 
+# Read in the two input files
 fn = "bank_data.csv"
-with open(fn,'rb') as f:
-    reader = csv.reader(f)
-    banknamesShort = [line[2].lower() for line in reader if line[2] != "Bank_Name"]
 fn2 = "call_report_names_unique.csv"
-with open(fn2,'rb') as f:
-    reader = csv.reader(f)
-    banknamesLong = [line[1].lower() for line in reader if line[2] != "rssd9348"]
-
-banknamesLong = removeDuplicates(banknamesLong)
-banknamesShort = removeDuplicates(banknamesShort)
-
-LETTERS = list(set("".join(banknamesLong + banknamesShort)))
-threshold = 3
-
 with Timer() as t:
+    with open(fn,'rb') as f:
+        reader = csv.reader(f)
+        banknamesShort = [line[2].lower() for line in reader if line[2] != "Bank_Name"]
+    with open(fn2,'rb') as f:
+        reader = csv.reader(f)
+        banknamesLong = [line[1].lower() for line in reader if line[2] != "rssd9348"]
+
+    banknamesLong = removeDuplicates(banknamesLong)
+    banknamesShort = removeDuplicates(banknamesShort)
+        
+    #set of all letters used in strings
+    LETTERS = list(set("".join(banknamesLong + banknamesShort)))
+    
+    #pick threshold
+    threshold = 3
+
+    #
     i = 0
     bins = []
     while i  < len(LETTERS):
@@ -100,16 +89,9 @@ with Timer() as t:
         binMatches[keyShort] = list(binDictLongKeys[np.sum(abs(binDictLongKeys - np.array(keyShort)),1) <= threshold])
         nameMatchLen[keyShort] = len(list(itertools.chain.from_iterable(map(binDictLong.get, [tuple(x) for x in binMatches[keyShort]]))))
 
-
 print "Bin Stage: %s seconds" % t.secs
 
-
-# matches = nameMatchLen.values()
-# n, bins, patches = plt.hist(matches, max(matches)-min(matches), normed=1, facecolor='green', alpha=0.5, align="left")            
-# pylab.savefig('numStringMatches.pdf', bbox_inches=0)
-# plt.close()
-
-
+# Use bitwise comparison to compare strings
 counter = 0
 successCounter = 0
 with Timer() as t:
@@ -117,6 +99,7 @@ with Timer() as t:
         # Gets all words from the long list in the bins that
         # are in the ball close to the keyShort tuple.
         compareWords = list(itertools.chain.from_iterable(map(binDictLong.get, [tuple(x) for x in binMatches[keyShort]])))
+        # Make the comparison words into bit strings
         compareWordsBits = [makeBit(word2, bitDict) for word2 in compareWords]
         for shortWord in binDictShort[keyShort]:
             shortWordBits = makeBit(shortWord, bitDict)
@@ -137,14 +120,6 @@ print "Number of Long Words: %d" % len(banknamesLong)
 print "Theoretical Number of Matches (short x long): %d" % (len(banknamesShort) * len(banknamesLong))
 print "Number of Total Comparisons: %d" % counter
 print "Number of Potential Matches: %d" % successCounter
-#testword = binDictShort[keyShort][0]
-# for word2 in compareWords:
-#     if bitCount(makeBit(testword, bitDict) ^ makeBit(word2, bitDict)) <= threshold:
-#         print "Comparing %s and %s" % (testword, word2)
-#         print "Bit Representations:"
-#         print "%s" % (str(bin(makeBit(testword, bitDict)))[2:])
-#         print "%s" % (str(bin(makeBit(word2, bitDict)))[2:])
-#         print "Difference: %d" % bitCount(makeBit(testword, bitDict) ^ makeBit(word2, bitDict))
 
 
 
